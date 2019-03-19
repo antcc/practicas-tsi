11ef969862a2ed231fb835b2cb323fc7e1debd04
Pablo <pbaeyens31@gmail.com>
2019-03-19 13:53:07 +0100

    Separate into functions

--- src/practica_busqueda/Agent.java
+++ src/practica_busqueda/Agent.java
@@ -16,145 +16,139 @@ import tools.pathfinder.PathFinder;
 
 /**
  * Agent class
- * @author Antonio Coín
+ * @author Antonio Coín y Pablo Baeyens
  */
 public class Agent extends BaseAgent {
-    
-    // Agent mode: 1 = random, 2 = basic A*
-    protected static int mode = 2;
-    
-    // Random agent
-    private Random randomGenerator;
-    
+
+  // Basic A* agent
+  private PathFinder pf;
+  private ArrayList<Node> path = new ArrayList<>();
+  private PlayerObservation ultimaPos;
+
+  public Agent(StateObservation so, ElapsedCpuTimer elapsedTimer) {
+    super(so, elapsedTimer);
+
     // Basic A* agent
-    private PathFinder pf;
-    private ArrayList<Node> path = new ArrayList<>();
-    private PlayerObservation ultimaPos;
-    
-    public Agent(StateObservation so, ElapsedCpuTimer elapsedTimer) {
-        super(so, elapsedTimer);
-        
-        // Random agent
-        if (mode == 1) {
-            randomGenerator = new Random();
-        }
-        
-        // Basic A* agent
-        else if (mode == 2) {
-            // Set obstacle types
-            ArrayList<Integer> tiposObs = new ArrayList();
-            tiposObs.add(0);  // <- Muros
-            tiposObs.add(7);  // <- Piedras
-            
-            // Init pathfinder
-            pf = new PathFinder(tiposObs);
-            pf.run(so);
-            
-            // Get last known position
-            ultimaPos = getPlayer(so);   
-        }
-    }
-    
-    @Override
-    public Types.ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
-        switch (mode) {
-            case 1:  // Random agent
-                return actRandom(stateObs, elapsedTimer);
-            case 2:  // Basic A* agent
-                return actBasicAStar(stateObs, elapsedTimer);
-            default:
-                return Types.ACTIONS.ACTION_NIL;
-        }
-    }
-    
-    /**
-     * *********************************************
-     * Test act methods
-     * *********************************************
-     */
-    
-    // Random agent act method
-private Types.ACTIONS actRandom(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
-        //Get the available actions in this game.
-        ArrayList<Types.ACTIONS> actions = stateObs.getAvailableActions();
+    // Set obstacle types
+    ArrayList<Integer> tiposObs = new ArrayList<>();
+    tiposObs.add(0);  // <- Muros
+    tiposObs.add(7);  // <- Piedras
 
-        //Determine an index randomly and get the action to return.
-        int index = randomGenerator.nextInt(actions.size());
-        Types.ACTIONS action = actions.get(index);
+    // Init pathfinder
+    pf = new PathFinder(tiposObs);
+    pf.run(so);
 
-        //Return the action.
+    // Get last known position
+    ultimaPos = getPlayer(so);
+  }
+
+  /*  public boolean shouldEscape(StateObservation stateObs){
+      System.out.println("Peligro");
+      Node next = path.get(0);
+      StateObservation newStateObs = stateObs.copy();
+      newStateObs.advance(getAction(ultimaPos,next));
+      return !newStateObs.isAvatarAlive();
+  }
+
+  public Types.ACTIONS escape(StateObservation stateObs){
+
+    for (Node vecino : pf.getNeighbours(ultimaPos)){
+      StateObservation newStateObs = stateObs.copy();
+      Types.ACTIONS action = getAction(ultimaPos, vecino);
+      newStateObs.advance(action);
+      if(newStateObs.isAvatarAlive())
         return action;
     }
-    
-    // Basic A* agent act method
-    private Types.ACTIONS actBasicAStar(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
-        // Set default action
-        Types.ACTIONS action = Types.ACTIONS.ACTION_NIL;
-        
-        // Get current position and clear path if needed
-        PlayerObservation avatar = getPlayer(stateObs);
-        if (((avatar.getX() != ultimaPos.getX()) || (avatar.getY() != ultimaPos.getY()))
-                && !path.isEmpty()) {
-            path.remove(0);
-        }
-        
-        // Get current gem count
-        int gems = getNumGems(stateObs);
-        
-        // Update path
-        if (path.isEmpty()) {
-            // Look for the exit (all gems collected)
-            if (gems == NUM_GEMS_FOR_EXIT) {
-                // Select nearest exit
-                ArrayList<core.game.Observation>[] exitList
-                        = stateObs.getPortalsPositions(stateObs.getAvatarPosition());
-                Observation exit = new Observation(exitList[0].get(0), stateObs.getBlockSize());
-                
-                // Calculate shortest path to nearest exit
-                path = pf.getPath(new Vector2d(avatar.getX(), avatar.getY()), 
-                                  new Vector2d(exit.getX(), exit.getY()));
-            }
-            
-            // Look for another gem
-            else {
-                // Select nearest gem
-                ArrayList<core.game.Observation>[] gemList
-                        = stateObs.getResourcesPositions(stateObs.getAvatarPosition());
-                Observation gem = new Observation(gemList[0].get(0), stateObs.getBlockSize());
-                
-                // Calculate shortest path to nearest exit
-                path = pf.getPath(new Vector2d(avatar.getX(), avatar.getY()), 
-                                  new Vector2d(gem.getX(), gem.getY()));
-            }
-        }
-        
-        // Calculate next action
-        if (path != null) {
-            Node siguientePos = path.get(0);
-            
-            if(siguientePos.position.x != avatar.getX()) {
-                if (siguientePos.position.x > avatar.getX()) {
-                    action = Types.ACTIONS.ACTION_RIGHT;
-                }
-                
-                else {
-                    action = Types.ACTIONS.ACTION_LEFT;
-                }
-            }
-            
-            else {
-                if(siguientePos.position.y > avatar.getY()) {
-                    action = Types.ACTIONS.ACTION_DOWN;
-                }
-                
-                else {
-                    action = Types.ACTIONS.ACTION_UP;
-                }
-            }
-            
-            ultimaPos = avatar;
-        }
-        
-        return action;
+    return Types.ACTIONS.ACTION_NIL;
+  }*/
+
+  /**
+   * *********************************************
+   * Test act methods
+   * *********************************************
+   */
+
+  /**
+   * Get action to get from a node to a different node
+   * @param from the origin node
+   * @param to the destination node
+   * @return the action to get from `from.position` to `to.position`.
+   */
+  private Types.ACTIONS getAction(Node from, Node to){
+    if(to.position.x != from.position.x) {
+      if (to.position.x > from.position.x) {
+        return Types.ACTIONS.ACTION_RIGHT;
+      } else {
+        return Types.ACTIONS.ACTION_LEFT;
+      }
     }
+    else {
+      if(to.position.y > from.position.y) {
+        return Types.ACTIONS.ACTION_DOWN;
+      } else {
+        return Types.ACTIONS.ACTION_UP;
+      }
+    }
+  }
+
+  /**
+   * Calculate a new path
+   * @param stateObs The current state
+   */
+  private void calculateNewPath(StateObservation stateObs){
+    PlayerObservation avatar = getPlayer(stateObs);
+    // Get current gem count
+    int gems = getNumGems(stateObs);
+    // Look for the exit (all gems collected)
+    if (gems == NUM_GEMS_FOR_EXIT) {
+      // Select nearest exit
+      ArrayList<core.game.Observation>[] exitList
+        = stateObs.getPortalsPositions(stateObs.getAvatarPosition());
+      Observation exit = new Observation(exitList[0].get(0), stateObs.getBlockSize());
+
+      // Calculate shortest path to nearest exit
+      path = pf.getPath(new Vector2d(avatar.getX(), avatar.getY()),
+        new Vector2d(exit.getX(), exit.getY()));
+    }
+
+    // Look for another gem
+    else {
+      // Select nearest gem
+      ArrayList<core.game.Observation>[] gemList
+        = stateObs.getResourcesPositions(stateObs.getAvatarPosition());
+      Observation gem = new Observation(gemList[0].get(0), stateObs.getBlockSize());
+
+      // Calculate shortest path to nearest exit
+      path = pf.getPath(new Vector2d(avatar.getX(), avatar.getY()),
+        new Vector2d(gem.getX(), gem.getY()));
+    }
+  }
+
+  // Basic A* agent act method
+  @Override
+  public Types.ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
+    // Set default action
+    Types.ACTIONS action = Types.ACTIONS.ACTION_NIL;
+
+    // Get current position and clear path if needed
+    PlayerObservation avatar = getPlayer(stateObs);
+    if (((avatar.getX() != ultimaPos.getX()) || (avatar.getY() != ultimaPos.getY()))
+      && !path.isEmpty()) {
+      path.remove(0);
+    }
+    ultimaPos = avatar;
+
+    // Update path
+    if (path.isEmpty()) {
+      calculateNewPath(stateObs);
+    }
+
+    // Calculate next action
+    if (path != null) {
+      Node siguientePos = path.get(0);
+      action = getAction(new Node(new Vector2d(avatar.getX(), avatar.getY())), siguientePos);
+    }
+
+    return action;
+  }
 }
